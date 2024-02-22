31979999bf33220bba295dfcdc54a936f999b83d
niaziiii <muhabatkhanx@gmail.com>
2024-02-22 11:19:05 +0500

    static page and social links added

--- src/components/social/index.tsx
+++ src/components/social/index.tsx
@@ -9,33 +9,33 @@ const SocialItems = () => {
     <div className="flex items-center justify-between w-full px-8">
       <div className="flex items-center gap-2">
         <a
-          href="#"
+          href="https://www.facebook.com/writroai?mibextid=ZbWKwL"
           className="text-xl h-8 w-8 bg-white rounded-full text-black flex items-center justify-center"
         >
           <FaFacebook />
         </a>
         <a
-          href="#"
+          href="https://www.instagram.com/writroai?igsh=MTljZXl2MHluenl1Zw=="
           className="text-xl h-8 w-8 bg-white rounded-full text-black flex items-center justify-center"
         >
           <FaInstagram />
         </a>
         <a
-          href="#"
+          href="https://x.com/WritroAI?t=1mgsu_NtVmcdxs3TKCwnwg&s=09"
           className="text-xl h-8 w-8 bg-white rounded-full text-black flex items-center justify-center"
         >
           <FaTwitter />
         </a>
         <a
-          href="#"
+          href="https://www.pinterest.com/Writroai"
           className="text-xl h-8 w-8 bg-white rounded-full text-black flex items-center justify-center"
         >
           <FaPinterest />
         </a>
       </div>
       <div className="flex items-center gap-4 text-white">
-        <a href="#">Declaimer</a>
-        <a href="#">Privacy Policy</a>
+        <a href="/disclaimer">Disclaimer</a>
+        <a href="/privacy">Privacy Policy</a>
         <a href="#">Terms of Service</a>
         <a href="#">About Us</a>
       </div>
